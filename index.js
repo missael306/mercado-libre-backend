e713c5f01bf167868086ae902c2ee1d2fa6c85f0
Missael Armenta <missael.armenta@uvmnet.edu>
2023-05-23 15:08:42 -0600

    Products routes config

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const routerApi = require('./routes/indexRouter');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
+routerApi(app);
 
 app.get('/', (req, res) => {
     res.send('Backend is running');
