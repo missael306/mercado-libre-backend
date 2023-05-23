42dad7780709bae447e0f71083b6737d10230ee5
Missael Armenta <missael.armenta@uvmnet.edu>
2023-05-23 14:46:06 -0600

    Initial express config

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log('Hello World');
+const express = require('express');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Backend is running');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
