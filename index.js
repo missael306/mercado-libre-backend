c94786cc1434cc42b1e5c608d85df05a2b8c669e
Missael Armenta <missael.armenta@uvmnet.edu>
2023-05-24 11:05:30 -0600

    Cors init config

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 const express = require('express');
 const routerApi = require('./routes/indexRouter');
 const { errorHandler , boomErrorHandler} = require('./middlewares/errorHandler');
+const cors = require('cors');
+require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
+
+const whitelist = process.env.ALLOW_ORIGINS;
+const options = {
+  origin: (origin, callback) => {
+    if (whitelist.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Origin not allowed by CORS'));
+    }
+  }
+}
+
+app.use(cors(options));
 routerApi(app);
 app.use(boomErrorHandler);
 app.use(errorHandler);
