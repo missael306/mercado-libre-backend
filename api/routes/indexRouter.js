76dd8975bc313ba6a9087b2c39ddd2ad9d2cc9c4
Missael Armenta <missael.armenta@uvmnet.edu>
2024-04-02 08:45:51 -0600

    Test sisu

--- api/routes/indexRouter.js
+++ api/routes/indexRouter.js
@@ -1,11 +1,13 @@
 const express = require('express');
 
 const productsRouter = require('./productsRouter');
+const sisuRouter = require('./sisuRouter');
 
 function routerApi(app) {
     const router = express.Router();
     app.use('/api', router);
     router.use('/items', productsRouter);
+    router.use('/sisu', sisuRouter);
 }
 
 module.exports = routerApi;
