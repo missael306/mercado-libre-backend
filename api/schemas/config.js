ba385eee6dd9d2e09095538f05740c5e8cea069a
Missael Armenta <missael.armenta@uvmnet.edu>
2024-04-02 15:48:58 -0600

    Allow localhost

--- api/schemas/config.js
+++ api/schemas/config.js
@@ -4,7 +4,7 @@ const config = {
     AUTHOR_NAME: (process.env.AUTHOR_NAME) ? process.env.AUTHOR_NAME : "Missael",
     AUTHOR_LASTNAME: (process.env.AUTHOR_LASTNAME) ? process.env.AUTHOR_LASTNAME : "Armenta",
     PRODUCTS_URL: (process.env.PRODUCTS_URL) ? process.env.PRODUCTS_URL : "https://api.mercadolibre.com/sites/MLA/search?q=",
-    ALLOW_ORIGINS: (process.env.ALLOW_ORIGINS) ? process.env.ALLOW_ORIGINS : "['http://localhost:3001','https://missael306.github.io']",
+    ALLOW_ORIGINS: (process.env.ALLOW_ORIGINS) ? process.env.ALLOW_ORIGINS : "['http://localhost:3001','https://missael306.github.io','http://localhost:5173','http://localhost']",
     PRODUCT_DETAIL: (process.env.PRODUCT_DETAIL) ? process.env.PRODUCT_DETAIL : "https://api.mercadolibre.com/items/",
     CATEGORY_URL: (process.env.CATEGORY_URL) ? process.env.CATEGORY_URL : "https://api.mercadolibre.com/categories/",       
     REGISTER_SAVE_MESSAGE: (process.env.REGISTER_SAVE_MESSAGE) ? process.env.REGISTER_SAVE_MESSAGE : "Usuario creado",
